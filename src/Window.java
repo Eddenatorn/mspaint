42499a246431b16ebdde1bcb0fbbd9251e419d6c
Edwin Segerstedt <edwin.segerstedt@learnet.se>
2023-12-04 09:18:16 +0100

    Initial commit

---
+++ src/Window.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Window {
+        
+        JFrame window = new JFrame();
+        this.setVisible(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setBounds(500, 0, 400, 510);
+        this.setResizable(false);
+        this.setLayout(null);
+        this.setTitle("MsPaint2.0");
+
+}
